7a0bbead2e929796ac64b54ec3522794239ab6c8
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-11-05 12:04:20 +0100

    init backend api project

---
+++ api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace shitcoin_analysis_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
