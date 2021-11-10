3cd57607f0efdfa9a9ea15326f2cf3b302501162
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-11-10 15:01:20 +0100

    - add swagger api docs to analytics service - fix nestjs build issue - fix index.cshtml typo

--- analytics/src/main.ts
+++ analytics/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+(async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  const config = new DocumentBuilder()
+    .setTitle('Shitcoin Analysis')
+    .setDescription('TAnalytics Service API')
+    .addTag('Endpoints')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT || 3000);
+})();
