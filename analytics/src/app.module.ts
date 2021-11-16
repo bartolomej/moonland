71d67df905c652a15045867a186e603dbdd60ebd
Bartolomej Kozorog <bartolomej.kozorog@gmail.com>
2021-11-16 13:05:03 +0100

    - add mariadb service - add db config for development

--- analytics/src/app.module.ts
+++ analytics/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoinsModule } from './coins/coins.module';
 import { SocialModule } from './social/social.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeorm } from './config';
 
 @Module({
-  imports: [CoinsModule, SocialModule],
+  imports: [CoinsModule, SocialModule, TypeOrmModule.forRoot(typeorm)],
   controllers: [AppController],
   providers: [AppService],
 })
--- analytics/src/config.ts
+++ analytics/src/config.ts
@@ -1,11 +1,19 @@
 import IClientOptions from 'twitter-api-client/dist/base/IClientOptions';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const {
   COINMARKETCAP_API_KEY,
+
   TWITTER_API_KEY,
   TWITTER_API_SECRET,
   TWITTER_ACCESS_TOKEN,
   TWITTER_ACCESS_TOKEN_SECRET,
+
+  DB_HOST,
+  DB_PORT,
+  DB_DATABASE,
+  DB_USER,
+  DB_PASSWORD,
 } = process.env;
 
 export const coinMarketCap = {
@@ -17,4 +25,15 @@ export const twitter: IClientOptions = {
   apiSecret: TWITTER_API_SECRET,
   accessToken: TWITTER_ACCESS_TOKEN,
   accessTokenSecret: TWITTER_ACCESS_TOKEN_SECRET,
+};
+
+export const typeorm: TypeOrmModuleOptions = {
+  type: 'mariadb',
+  host: DB_HOST,
+  port: parseInt(DB_PORT),
+  password: DB_PASSWORD,
+  username: DB_USER,
+  database: DB_DATABASE,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  synchronize: true, // disable in stable production environment
 };
