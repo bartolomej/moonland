9fbb3f6525ac3f6db0550176c8a1a6e724b37566
bukev123@gmail.com <bukev123@gmail.com>
2022-01-11 01:11:38 +0100

    Made a framework for graphs

---
+++ gateway/Models/CoinStats.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace gateway.Models
+{
+    public class CoinStats
+    {
+        public string label { get; set; }
+        public int value { get; set; }
+
+        public string symbol { get; set; }
+    }
+}
